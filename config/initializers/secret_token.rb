96ac622ac34e38939d011c68377bad3f91f9b0e3
Hunter <hunter@iReallyUbuntu.(none)>
2013-12-19 19:35:55 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoRails::Application.config.secret_key_base = '933b813b3161ac558ab020042785d4ddde71038e8f90f8b4b7f967dc614dbe235b4cd9f1c5f7eb594bbbb64f71c9d96c1530a398f0f96532982e8734a259ec87'
